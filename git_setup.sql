7bbd719eddd2f39de20f70015da2cbed1e3836ed
sfc-gh-yyano <yusuke.yano@snowflake.com>
2026-04-01 17:37:42 +0900

    Update git_setup.sql

--- git_setup.sql
+++ git_setup.sql
@@ -1,1 +1,14 @@
+-- コンテキストを指定してデータベースを切り替える
+ SET my_db = 'USER$' || CURRENT_USER();                                                              
+ USE DATABASE IDENTIFIER($my_db);  
 
+-- GitHubリポジトリと連携するためのAPI統合を作成
+CREATE OR REPLACE API INTEGRATION git_api_integration
+ API_PROVIDER = git_https_api
+ API_ALLOWED_PREFIXES = ('https://github.com/sfc-gh-yyano/')
+ ENABLED = TRUE;
+
+-- ハンズオン用のGitHubリポジトリを登録
+CREATE OR REPLACE GIT REPOSITORY car_insurance_handson
+ API_INTEGRATION = git_api_integration
+ ORIGIN = 'https://github.com/sfc-gh-yyano/car_insurance_handson.git';
